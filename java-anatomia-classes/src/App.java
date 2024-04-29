225bd0ab451a0963df1e5e13d99393f1efbae24c
profpedrosa <pedrosaprof@gmail.com>
2024-04-29 20:31:33 -0300

    Acrescentado exemplo de método

--- java-anatomia-classes/src/App.java
+++ java-anatomia-classes/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Olá turma, sejam bemvindos!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro nome:");
+        String primeiroNone = sc.next();
+        System.out.print("Digite o Segundo nome: ");
+        String segundoNome = sc.next();
+
+        String nomeCompleto = nomeCompleto(primeiroNone, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+        sc.close();
+    }
+
+    public static String nomeCompleto(String primeiroNome, String SegundoNome){
+        return primeiroNome.concat(" ").concat(SegundoNome);
     }
 }
