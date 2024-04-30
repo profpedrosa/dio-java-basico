b722ec000031558197877fb1ba4b5f9b70e174e0
profpedrosa <pedrosaprof@gmail.com>
2024-04-29 22:29:27 -0300

    Modificando método

--- java-anatomia-classes/src/App.java
+++ java-anatomia-classes/src/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite o primeiro nome:");
+        System.out.print("Digite o primeiro nome: ");
         String primeiroNone = sc.next();
         System.out.print("Digite o Segundo nome: ");
         String segundoNome = sc.next();
@@ -16,6 +16,6 @@ public class App {
     }
 
     public static String nomeCompleto(String primeiroNome, String SegundoNome){
-        return primeiroNome.concat(" ").concat(SegundoNome);
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(SegundoNome);
     }
 }
